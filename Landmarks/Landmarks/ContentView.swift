2122a6f989e7f331e1abc38de720e5d81b165aca
Jordan Wood <wjordanwood@gmail.com>
2019-10-09 10:01:35 -0500

    Added Landmarks Project
    based on the tutorial at https://developer.apple.com/tutorials/swiftui/creating-and-combining-views

---
+++ Landmarks/Landmarks/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by Jordan Wood on 10/9/19.
+//  Copyright © 2019 Jordan Wood. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
